bef36760f6365cea39db27a84891715a3fae773f
rzrbld <razblade@gmail.com>
2017-01-12 03:48:55 +0300

    update backend addr

--- Arduino/SDK-Project/dth11_wifi_ESP8266.ino
+++ Arduino/SDK-Project/dth11_wifi_ESP8266.ino
@@ -1,9 +1,9 @@
 #include "DHT.h"                                                   // include DHT lib for humidity and temperature sensor
-#include "LiquidCrystal.h"                                         // include LCD lib for 1602 LCD diplay	
-#include "SoftwareSerial.h"					   // include	Software Serial lib for ESP8266 
+#include "LiquidCrystal.h"                                         // include LCD lib for 1602 LCD diplay
+#include "SoftwareSerial.h"					   // include	Software Serial lib for ESP8266
 
 #define DHTPIN 7                                                   // DHT11 pin
-#define DHTTYPE DHT11                                              // DHT 11 
+#define DHTTYPE DHT11                                              // DHT 11
 
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);                         //lcd pins
 SoftwareSerial esp8266(8, 9);                                      //esp pins
@@ -13,13 +13,13 @@ int photocellReading;                                              // the analog
 
 int dropDataCounter = 0;                                           // data sender counter
 
-int led = 13;                                                      // build-in led 
+int led = 13;                                                      // build-in led
 
 DHT dht(DHTPIN, DHTTYPE);                                          // init DHT 11
 
-//======================Configure 
-String backendServerIP = "185.46.11.15";                            // backend serer IP rzrbld.ru
-String backendServerPort = "3000";                                  // backend server PORT
+//======================Configure
+String backendServerIP = "176.58.108.193";                            // backend serer IP rzrbld.ru
+String backendServerPort = "4000";                                  // backend server PORT
 String wifiSSID = "myWiFi";                                         // WiFi SSID name
 String wifiPassword = "thelongestwifipasswordontheearth";                                       // WiFi password
 String arduinoUUID = "d4e4016e-63a4-4f65-812a-37d8a8011a09";        // WARN! generate new before upload to arduino
@@ -32,7 +32,7 @@ void setup() {
   pinMode(led, OUTPUT);
   dht.begin();                                          // start humidity sensor
   lcd.begin(16, 2);                                     // start lcd screen
-  updateLoadingScreen(1, "[i] start v0.1  ", "[INFO] start v0.1");    
+  updateLoadingScreen(1, "[i] start v0.1  ", "[INFO] start v0.1");
   digitalWrite(led, LOW);                               // turn off builtin arduino led
 
   esp8266.println("AT+RST");                            // reset ESP8266 module
@@ -47,13 +47,13 @@ void setup() {
   }
   delay(500);                                           // wait more
                                                         // connect to the wifi
-  boolean connected = false;				// satate 
+  boolean connected = false;				// satate
   for (int i = 0; i < 5; i++)                           // try 5 times to connect
   {
     if (connectWiFi()) {				// if connected
       connected = true;                                 // set state = true
       updateLoadingScreen(3, "[i] WiFi cncted ", "[INFO] WiFi connected.");
-      break;						// break 
+      break;						// break
     }
   }
   if (!connected) {					// if still not connected
@@ -62,22 +62,22 @@ void setup() {
   }
   delay(5000);						// wait 5 sec
   updateLoadingScreen(4, "[i] Obtain IP   ", "[INFO] Obtain IP address.");
-  
-  esp8266.println("AT+CIFSR");				// try to get IP value. not critical. [WIP] 
+
+  esp8266.println("AT+CIFSR");				// try to get IP value. not critical. [WIP]
   Serial.println(esp8266.read());
-  																
+
   esp8266.println("AT+CIPMUX=0");			// set the single connection mode
   updateLoadingScreen(5, "[i] CIPMUX:0    ", "[INFO] Set CIPMUX:0.");
   delay(1000);						// wait a sec
   esp8266.println("AT+CIPMODE=0");			// set "not data mode" duplex mode (get/send data)
   updateLoadingScreen(6, "[i] CIPMODE:0   ", "[INFO] Set CIPMODE:0.");
-  delay(1000);						// wait a sec	
+  delay(1000);						// wait a sec
   updateLoadingScreen(7, "[i] Post init   ", "[INFO] Post init");
-  delay(1000);						// wait a sec	
+  delay(1000);						// wait a sec
 }
 
 void loop() {
-  
+
   lcd.clear();
   photocellReading = analogRead(photocellPin);          //read photocell data
 
@@ -115,19 +115,19 @@ void loop() {
     cmd += backendServerIP;																					// IP address of backend server
     cmd += "\",";
     cmd += backendServerPort;																								// backend server PORT
-    esp8266.println(cmd);																						// send connection command to ESP8266 
-    Serial.println(cmd);																						// serial debug out 
+    esp8266.println(cmd);																						// send connection command to ESP8266
+    Serial.println(cmd);																						// serial debug out
     if (esp8266.find("Error")) {			// error connection
       return;																												// return;
-    }																																	
+    }
     cmd = "POST /data/";   																							// building POST request
     cmd += arduinoUUID;
     cmd += "/t/";
     cmd += t;																												// --
     cmd += "/h/";																										// --
     cmd += h;																												// --
-    cmd += "/l/";																										// --		
-    cmd += photocellReading;			         // --			
+    cmd += "/l/";																										// --
+    cmd += photocellReading;			         // --
     cmd += " HTTP/1.0\r\nHost: test.iot.domain\r\nConnection: close\r\n\r\n";
     esp8266.print("AT+CIPSEND=");			// send sending command to ESP8266
     esp8266.println(cmd.length());			// calculate length of the POST request
@@ -137,13 +137,13 @@ void loop() {
     } else																													// send mode error
     {
       esp8266.println("AT+CIPCLOSE");			// close connection
-      Serial.println("[WARN] Connect timeout. Wait 15 sec");// serial debug out 
+      Serial.println("[WARN] Connect timeout. Wait 15 sec");// serial debug out
       delay(15000);																									// wait 15 sec
       return;																												// return
     }
-    esp8266.print(cmd);																							// finaly send data to backend server	
-    Serial.print(cmd);																							// serial debug out 
-    delay(2000);																										// wait 2 sec			
+    esp8266.print(cmd);																							// finaly send data to backend server
+    Serial.print(cmd);																							// serial debug out
+    delay(2000);																										// wait 2 sec
 
     while (esp8266.available())				// read answer
     {
@@ -155,24 +155,24 @@ void loop() {
 }
 
 boolean connectWiFi()																								// wifi connection function
-{																																		
+{
   esp8266.println("AT+CWMODE=1");		        // set ESP8266 to STA mode: 1 = WiFi client, 2 = Soft AP (router), 3 = both.
   String cmd = "AT+CWJAP=\"";				// build connection string
   cmd += wifiSSID;																										// wifi network SSID, hidden supported
   cmd += "\",\"";																										// --
   cmd += wifiPassword;																							// wifi password
-  cmd += "\"";			
+  cmd += "\"";
   Serial.println(cmd);																								// --
-  Serial.println("[INFO] Connecting to wifi network");	// serial debug out 
+  Serial.println("[INFO] Connecting to wifi network");	// serial debug out
   esp8266.println(cmd);																							// send connection commant to ESP8266
   delay(5000);																											// connection established in 1-5 secs
   if (esp8266.find("OK"))																						// if connection success
   {
-    Serial.println("[INFO] Connected to WiFi.");        // serial debug out 
+    Serial.println("[INFO] Connected to WiFi.");        // serial debug out
     return true;																										// return true
   } else
   {
-    Serial.println("[ERROR] Can not connect to the WiFi.");	 // serial debug out 
+    Serial.println("[ERROR] Can not connect to the WiFi.");	 // serial debug out
     return false;																										// return false
   }
 }
